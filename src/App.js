36cf384ba2f1057d36a3efde5aa1e719c8944ded
oribar1 <oribar9243@gmail.com>
2023-02-18 16:53:40 +0200

    useState page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+
+  return (
+    <main>
+      <section className='container'>
+        <h4>{people.length} birthday today</h4>
+        <List people={people}></List>
+        <button onClick={() => setPeople([])}> clear all</button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
